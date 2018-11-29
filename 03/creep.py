9c3a926c89eaec2fc68f5704584ab86d95476f2f
Rudegolf <olivin.spacek@gmail.com>
2018-11-29 18:18:17 +0100

    vyroben šnek

---
+++ 03/creep.py
@@ -0,0 +1,41 @@
+from turtle import forward,left,right,exitonclick,color
+from random import choice
+
+
+
+for x in range(74):
+	forward(x / 3)
+	right(12)
+
+left(165)
+forward(140)	
+left(90)
+forward(70)	
+left(90)
+
+for x in range(30):
+	forward(3)	
+	right(12)
+	
+left(90)
+forward(70)
+left(90)	
+forward(50)	
+
+for x in range(15):
+	forward(3)
+	right(12)
+
+	
+forward(500)
+right(165)
+
+for x in range(8):
+	forward(30)
+	left(2)
+	
+exitonclick()
+
+
+
+
